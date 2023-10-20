1c033bff04d99d1a4c7adf54eb63baa4e86979a1
Karthik Shetty <karthiksshetty111@gmail.com>
2023-10-20 15:05:27 +0530

    complete level2 and env authentication and security done

---
+++ app.js
@@ -0,0 +1,115 @@
+//jshint esversion:6
+import 'dotenv/config';
+import express from 'express';
+import bodyParser from 'body-parser';
+import ejs from 'ejs';
+import mongoose from 'mongoose';
+import encrypt from 'mongoose-encryption';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+
+// Database configuration
+mongoose.connect('mongodb://localhost:27017/secretsDB');
+
+const userSchema = new mongoose.Schema({
+    email : {       
+            type : String, 
+            unique : true,
+            lowercase : true,
+            trim : true,
+            required : true,
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']      
+    },
+    password : {
+        type : String,
+        required : true
+    }
+})
+
+userSchema.plugin(encrypt, {secret : process.env.SECRETSTRING, encryptedFields : ["password"]});
+
+const User = mongoose.model('User', userSchema);
+
+//handlers
+app.get('/', (req, res) => {
+    res.render("home.ejs");
+}) 
+
+app.get('/register', (req, res) => {
+    res.render("register.ejs");
+})
+
+app.get('/login', (req, res) => {
+    res.render("login.ejs");
+})
+
+app.post('/register', (req, res) => {
+    
+    User.findOne({email : req.body.email})
+    .then(result => {
+        if(result) {
+            console.log("Email already exists. Try logging in");
+            res.render('register', {
+                mess : "Email already exists. Try logging in"
+            });
+
+        } else {
+            const userData = new User({
+                email : req.body.email,
+                password : req.body.password
+            });
+
+            userData.save()
+            .then(result => {
+                console.log(result + "\n New user has been created.");
+                res.redirect('/login');
+            })
+            .catch(err => {
+                if(err.name === "ValidationError") {
+                    res.render('register', {
+                        mess : err.message
+                    })
+                } else {
+                    console.log(err);
+                }
+            })
+        }
+    })
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({email : req.body.email})
+    .then(result => {
+        if(!result) {
+            res.render('register.ejs', {
+                mess : "Account doesn't exist. Please register yourself first!"
+            })
+        } else {
+            if(result.password === req.body.password){
+                res.render('secrets.ejs');
+                console.log("login succesfull");
+            } else {
+                res.render("login", {
+                    mess : "Password doesn't match!"
+                })
+            }
+        }
+    }).catch(err => {
+        console.log(err);
+    })
+})
+
+app.get('/logout', (req, res) => {
+    res.render('home.ejs')
+})
+
+app.listen(port, ()=> {
+    console.log('Server started at port ' + port);
+})
