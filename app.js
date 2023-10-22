817b31fca157c22420cf6c8867386b9b2bb9e0ea
Karthik Shetty <karthiksshetty111@gmail.com>
2023-10-22 14:43:09 +0530

    Complete passport.js authentication with cookies and sessions

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import ejs from 'ejs';
 import mongoose from 'mongoose';
-// import encrypt from 'mongoose-encryption';
-// import md5 from 'md5';
-import bcrypt from 'bcrypt';
+import session from 'express-session';
+import passport from 'passport';
+import passportLocalMongoose from 'passport-local-mongoose';
 
 const app = express();
 const port = process.env.PORT || 3000;
-const saltRound = 10;
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
@@ -18,27 +17,38 @@ app.use(bodyParser.urlencoded({
     extended : true
 }));
 
+app.use(session({
+    secret : "KarthikIsASuperHero",
+    resave : false,
+    saveUninitialized : false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 // Database configuration
 mongoose.connect('mongodb://localhost:27017/secretsDB');
 
 const userSchema = new mongoose.Schema({
     email : {       
             type : String, 
-            unique : true,
             lowercase : true,
             trim : true,
-            required : true,
             match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']      
     },
     password : {
         type : String,
-        required : true
     }
 })
 
-// userSchema.plugin(encrypt, {secret : process.env.SECRETSTRING, encryptedFields : ["password"]});
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model('User', userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 //handlers
 app.get('/', (req, res) => {
@@ -53,72 +63,66 @@ app.get('/login', (req, res) => {
     res.render("login.ejs");
 })
 
-app.post('/register', (req, res) => {  
-    User.findOne({email : req.body.email})
-    .then(result => {
-        if(result) {
-            console.log("Email already exists. Try logging in");
-            res.render('register', {
-                mess : "Email already exists. Try logging in"
-            });
-
-        } else {
-            bcrypt.hash(req.body.password, saltRound)
-            .then(pass => {
-                const userData = new User({
-                    email : req.body.email,
-                    // password : md5(req.body.password)
-                    password : pass
-                });
-
-            userData.save()
-            .then(result => {
-                console.log(result + "\n New user has been created.");
-                res.redirect('/login');
-            })
-            .catch(err => {
-                if(err.name === "ValidationError") {
-                    res.render('register', {
-                        mess : err.message
-                    })
-                } else {
-                    console.log(err);
-                }
-            })
-            })
-
-            
-        }
-    })
+app.get('/secrets', (req, res) => {
+    if(req.isAuthenticated()) {
+        res.render('secrets.ejs');
+    } else {
+        res.render('register.ejs', {mess : "Unauthorised access. First register and login to our website"});
+    }
 })
 
-app.post('/login', (req, res) => {
-    User.findOne({email : req.body.email})
+app.post('/register', (req, res) => {
+    console.log(req.body);
+    User.register({ username: req.body.username}, req.body.password)
     .then(result => {
-        if(!result) {
-            res.render('register.ejs', {
-                mess : "Account doesn't exist. Please register yourself first!"
-            })
+        if (result) {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect('/secrets');
+            });
         } else {
-            bcrypt.compare(req.body.password, result.password)
-            .then(result => {
-                if(result) {
-                    res.render('secrets.ejs');
-                    console.log("login succesfull");
-                }else {
-                    res.render("login", {
-                        mess : "Password doesn't match!"
-                    })
-                }
-            })
+            console.log(result);
+            res.redirect('/');
         }
-    }).catch(err => {
-        console.log(err);
+    })
+    .catch(err => {
+        if (err.name === "UserExistsError") {
+            res.render('register.ejs', { mess: "Account already exists. Try logging in" });
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post('/login', (req, res) => {
+    const user = new User({
+        username : req.body.username,
+        password : req.body.password
+    });
+
+    console.log(req.body);
+    req.login(user, (err) => {
+        try {
+            if(err) {
+                console.log(err);
+                res.render('login.ejs', {mess : err.message});
+            } else {
+                passport.authenticate('local')(req, res, ()=> {
+                    res.redirect('/secrets');
+                })
+            }
+        }catch(err) {
+            console.log(err);   
+        }
     })
 })
 
 app.get('/logout', (req, res) => {
-    res.render('home.ejs')
+    req.logout((err)=>{
+        if(err) {
+            console.log(err);
+        }
+    });
+    res.redirect('/')
 })
 
 app.listen(port, ()=> {
